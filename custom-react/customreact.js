1a41ad8c7603a4c351cc15cc3194f48320597582
gustang <vyasuday678@gmail.com>
2025-10-07 22:24:58 +0530

    custom react revision

--- custom-react/customreact.js
+++ custom-react/customreact.js
@@ -1,15 +1,14 @@
 function customRender(reactElement, container) {
   /*
-    const domElement = document.createElement(reactElement.type);
+  const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.Children;
   domElement.setAttribute("href", reactElement.props.href);
   domElement.setAttribute("target", reactElement.props.target);
 
   container.appendChild(domElement);
   */
-
   const domElement = document.createElement(reactElement.type);
-  domElement.innerHTML = reactElement.children;
+  domElement.innerHTML = reactElement.Children;
   for (const prop in reactElement.props) {
     if (prop === "children") continue;
     domElement.setAttribute(prop, reactElement.props[prop]);
@@ -23,7 +22,7 @@ const reactElement = {
     href: "https://google.com",
     target: "_blank"
   },
-  children: "Click me to visit google"
+  Children: "click me to visit google"
 };
 
 const mainContainer = document.querySelector("#root");
